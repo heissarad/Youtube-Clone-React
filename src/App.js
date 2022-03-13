dffaa76b1d3ebf358755b030bd64814eb9a13eff
sarad096 <saradgyawali096@gmail.com>
2022-03-13 21:39:49 +0545

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,42 +1,91 @@
-import Card from "./Card"
-import React from 'react'
-import Data from './Data'
-import Typewriter from "typewriter-effect"
-import "./index.css"
+import Card from "./Card";
+import React from 'react';
+import Data from './Data';
+import Typewriter from "typewriter-effect";
+import "./index.css";
 
 const App = () => {
-  const mydb = Data.map((Data)=>
-  {
-  return (
-    < Card
-    key = {Data.id}
-    img = {Data.cardimg}
-    title = {Data.title}
-    logoimg = {Data.logoimg}
-    channelname = {Data.channelname}
-    views = {Data.views}
-    date = {Data.date}
-    verified = {Data.verified}
-    url = {Data.url}
-    channelurl = {Data.channelurl}
+  const user_fav_mode = JSON.parse(localStorage.getItem('mode'));
 
-    />
-  )
+  const [mode, setMode] = React.useState(
+    user_fav_mode ? user_fav_mode :
+      {
+        isTrue: 1,
+        backgroundColor: "white",
+        color: "black"
+
+
+      }
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('mode', JSON.stringify(mode));
+  }, [mode]);
+
+
+
+
+  function handleOnClick() {
+    setMode((prevMode) => ({
+      ...prevMode,
+      isTrue: !mode.isTrue,
+      backgroundColor: mode.isTrue ? "black" : "white",
+      color: mode.isTrue ? "white" : "black"
+
+
+    })
+
+
+    );
+
+  }
+  document.body.style.backgroundColor = `${mode.backgroundColor}`;
+
+  const mydb = Data.map((Data) => {
+    return (
+      < Card
+        key={Data.id}
+        img={Data.cardimg}
+        title={Data.title}
+        logoimg={Data.logoimg}
+        channelname={Data.channelname}
+        views={Data.views}
+        date={Data.date}
+        verified={Data.verified}
+        url={Data.url}
+        channelurl={Data.channelurl}
+        time={Data.time}
+        mode={mode.backgroundColor}
+        color={mode.color}
+      />
+    );
   }
   );
-  return (
-    <div className="card-container">
-      <div className="tw-effect">
-      <Typewriter loop={true} onInit={(typewriter)=>{
-      typewriter.typeString("Presenting Youtube Clone").pauseFor(3000).deleteAll().typeString('Created By Sarad Gyawali').start();
-   }}
-   />
-        </div>
-<div className="card-comp">
-{mydb}
-</div>
-</div>
-  )
-}
 
-export default App
+
+
+  return (
+    <div className="card-container" >
+     
+      <span className="sun--logo" onClick={handleOnClick}>
+        {mode.isTrue ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-brightness-low-fill" viewBox="0 0 16 16">
+          <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8.5 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm5-5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm-11 0a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm9.743-4.036a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm-7.779 7.779a.5.5 0 1 1-.707-.707.5.5 0 0 1 .707.707zm7.072 0a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707zM3.757 4.464a.5.5 0 1 1 .707-.707.5.5 0 0 1-.707.707z" />
+        </svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+          <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
+          <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
+        </svg>}
+      </span>
+      <span className="tw-effect" style={{ color: `${mode.color}` }}>
+        <Typewriter loop={true} onInit={(typewriter) => {
+          typewriter.typeString("Presenting Youtube Clone").pauseFor(1000).deleteAll().typeString('Created By Sarad Gyawali').start();
+        }}
+        />
+      </span>
+      <div className="card-comp">
+        {mydb}
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,35 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 
 const Card = (props) => {
   return (
     <div className="cardbox">
-      <div className="card" style={{ width: "18rem", backgroundColor: "black" }}>
-        <a href={props.url} target='_blank'>
-        <img className="card-img-top" src={props.img} alt="Card image cap" />
+      <div className="card "style={{ backgroundColor: `${props.mode}` }} >
+        <a href={props.url} className="time1" target='_blank'>
+          <img className="card-img-top" src={props.img} alt="Card image cap" />
 
         </a>
-        < div className="card-body" style={{padding: "10px"}}>
+        {/* <div className="time">{props.time}</div> */}
+        < div className="card-body" style={{ backgroundColor: `${props.mode}` }} >
           <div className="header">
             <a href={props.channelurl} target='_blank'>
-            <img  className="logoimg" src={props.logoimg} alt="logoimg" />
+              <img className="logoimg" src={props.logoimg} alt="logoimg" />
             </a>
-            <a href={props.url} target = '_blank' className="video--url">
+            <a href={props.url} target='_blank' className="video-title-url">
 
-            <h6  className="card-title" style={{ textOverflow: "ellipsis"}}>{props.title}</h6>
+              <h6 className="card-title" style={{ color: `${props.color}` }} >{props.title}</h6>
             </a>
           </div>
-         <a href={props.channelurl} className="channelurl" target='_blank'>
-              <p className="card-text"> {props.channelname} {props.verified===1 &&<svg style={{color: "gray"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
+          <a href={props.channelurl} className="channel-name-url" target='_blank'>
+            <p className="card-text"> {props.channelname} {props.verified === 1 && <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-patch-check-fill" viewBox="0 0 16 16">
               <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
             </svg>}</p></a>
 
-         
-            <span className="card-text">{props.views} views<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
-              <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-            </svg>{props.date}</span>
 
-          
+          <span className="card-text">{props.views} views<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dot" viewBox="0 0 16 16">
+            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+          </svg>{props.date}</span>
+
+
         </div>
       </div>
     </div>
